1a5cb81ae7ef22208088c18021ca70bcc30bb44b
Calvin Liang <me@calvin.sh>
2020-11-28 15:37:40 -0800

    chore(examples): simplify resourceQuery in react

--- examples/react/webpack.config.js
+++ examples/react/webpack.config.js
@@ -7,31 +7,31 @@ module.exports = {
     path: path.resolve(__dirname, "./build"),
     filename: "main.js",
   },
+  // to hide performance hints on compile, do not add this to your code
+  performance: {
+    hints: false,
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: "babel-loader",
       },
       {
-        test: /\.(png|jpe?g)/i,
-        oneOf: [
-          {
-            resourceQuery: /placeholder/,
-            use: {
-              loader: "webpack-image-placeholder-loader",
-              options: {
-                format: "hex",
-              },
-            },
+        test: /\.(png|jpe?g|svg|gif|webp|tiff?)$/i,
+        resourceQuery: /placeholder/,
+        use: {
+          loader: "webpack-image-placeholder-loader",
+          options: {
+            format: "hex",
           },
-          {
-            use: {
-              loader: "file-loader",
-            },
-          },
-        ],
+        },
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif|webp|tiff?)$/i,
+        resourceQuery: { not: [/placeholder/] },
+        use: "file-loader",
       },
     ],
   },
