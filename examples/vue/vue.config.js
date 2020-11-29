7bf2b6c63a5e345be34ef1dbc035c20382033f12
Calvin Liang <me@calvin.sh>
2020-11-29 14:34:17 -0800

    chore(examples): remove webpack-query-loader from vue example

--- examples/vue/vue.config.js
+++ examples/vue/vue.config.js
@@ -2,33 +2,23 @@ module.exports = {
   publicPath: ".",
   chainWebpack: (config) => {
     config.module
-      .rule("images-placeholder")
-      .test(/\.(png|jpe?g|svg|gif|webp|tiff?)$/i)
-      .use("placeholder")
-      .loader("webpack-query-loader")
-      .options({
-        resourceQuery: "placeholder",
-        use: {
-          loader: "webpack-image-placeholder-loader",
-          options: { esModule: false },
-        },
-      });
-
-    config.module
       .rule("images")
-      .use("images-without-query")
-      .loader("webpack-query-loader")
-      .options({
-        resourceQuery: "!placeholder",
-        use: {
-          loader: config.module.rule("images").use("url-loader").get("loader"),
-          options: config.module
-            .rule("images")
-            .use("url-loader")
-            .get("options"),
-        },
-      });
+      .test(/\.(png|jpe?g|gif|webp|tiff?)$/i)
+      // if the import url looks like "some.png?placeholder..."
+      .oneOf("placeholder")
+      .resourceQuery(/placeholder/)
+      .use("placeholder")
+      .loader("webpack-image-placeholder-loader")
+      .options({ format: "hex", esModule: false })
+      .end()
+      .end()
+      // if no previous resourceQuery match
+      .oneOf("normal")
+      .use("normal")
+      .loader(config.module.rule("images").use("url-loader").get("loader"))
+      .options(config.module.rule("images").use("url-loader").get("options"));
 
+    // remove `use` now that there is `oneOf`
     config.module.rule("images").uses.delete("url-loader");
   },
 };
