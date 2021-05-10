b328c50ce323df9ff5ee8bb1095e35c60855258c
Calvin Liang <me@calvin.sh>
2021-05-09 18:57:14 -0700

    chore(examples): update examples

--- examples/react/src/App.js
+++ examples/react/src/App.js
@@ -2,69 +2,46 @@ import React from "react";
 
 import image from "./assets/Macaca_nigra_self-portrait_large.jpg";
 import imagePlaceholder from "./assets/Macaca_nigra_self-portrait_large.jpg?placeholder";
-import './assets/example.css'
+import imageBlurredPlaceholder from "./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=blurred-svg&blurQuality=0.5";
 
 function App() {
   return (
-    <div> 
-      <div>
+    <div>
       <img
-        style={{ width: 728, height: 1007, backgroundColor: imagePlaceholder }}
+        style={{
+          width: 728,
+          height: 1007,
+          backgroundColor: imagePlaceholder,
+        }}
         src={image}
       />
+
       <img
         // you can also use require
+        style={{
+          width: 728,
+          height: 1007,
+        }}
         src={require("./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&size=original&format=base64&esModule=false")}
       />
-    </div>
 
-      <div class="blur-container">
-        <h2 class="blur-title">Svg blur</h2>
-        <div class="blur-grid">
-          <div class="blur-grid-item">
-            <img
-              class="blur-grid-item__img"
-              style={{
-                backgroundColor: require('./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=10&esModule=false'),
-              }}
-              src={image}
-            />
-            <span class="blur-grid-item__desc">Original</span>
-          </div>
+      <img
+        style={{
+          width: 728,
+          height: 1007,
+          backgroundSize: "cover",
+          backgroundImage: `url("${imageBlurredPlaceholder}")`,
+        }}
+        src={image}
+      />
 
-          <div class="blur-grid-item">
-            <img
-              class="blur-grid-item__img"
-              src={require('./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=100&esModule=false')}
-            />
-            <span class="blur-grid-item__desc">blurQuality=100</span>
-          </div>
-
-          <div class="blur-grid-item">
-            <img
-              class="blur-grid-item__img"
-              src={require('./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=50&esModule=false')}
-            />
-            <span class="blur-grid-item__desc">blurQuality=50</span>
-          </div>
-
-          <div class="blur-grid-item">
-            <img
-              class="blur-grid-item__img"
-              src={require('./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=10&esModule=false')}
-            />
-            <span class="blur-grid-item__desc">blurQuality=10</span>
-          </div>
-
-          <div class="blur-grid-item">
-            <img
-              class="blur-grid-item__img"
-              src={require('./assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=0.5&esModule=false')}
-            />
-            <span class="blur-grid-item__desc">blurQuality=0.5</span>
-          </div>
-        </div>
-      </div>
+      <img
+        style={{
+          width: 728,
+          height: 1007,
+        }}
+        src={imageBlurredPlaceholder}
+      />
     </div>
   );
 }
--- examples/vue/src/App.vue
+++ examples/vue/src/App.vue
@@ -8,94 +8,24 @@
       src="@/assets/Macaca_nigra_self-portrait_large.jpg"
     />
     <img
+      style="width: 728px; height: 1007px"
       :src="
         require('@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&size=original&format=base64&esModule=false')
       "
     />
-  </div>
+    <img
+      style="width: 728px; height: 1007px"
+      :style="{
+        backgroundImage: `url('${require('@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=blurred-svg&blurQuality=0.5')}')`,
+      }"
+      src="@/assets/Macaca_nigra_self-portrait_large.jpg"
+    />
 
-  <div class="blur-container">
-    <h2 class="blur-title">Svg blur</h2>
-    <div class="blur-grid">
-      <div class="blur-grid-item">
-        <img
-          class="blur-grid-item__img"
-          :style="{
-            backgroundImage: require('@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=10'),
-          }"
-          src="@/assets/Macaca_nigra_self-portrait_large.jpg"
-        />
-        <span class="blur-grid-item__desc">Original</span>
-      </div>
-
-      <div class="blur-grid-item">
-        <img
-          class="blur-grid-item__img"
-          src="@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=100"
-        />
-        <span class="blur-grid-item__desc">blurQuality=100</span>
-      </div>
-
-      <div class="blur-grid-item">
-        <img
-          class="blur-grid-item__img"
-          src="@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=50"
-        />
-        <span class="blur-grid-item__desc">blurQuality=50</span>
-      </div>
-
-      <div class="blur-grid-item">
-        <img
-          class="blur-grid-item__img"
-          src="@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=10"
-        />
-        <span class="blur-grid-item__desc">blurQuality=10</span>
-      </div>
-
-      <div class="blur-grid-item">
-        <img
-          class="blur-grid-item__img"
-          src="@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=svg-blur&blurQuality=0.5"
-        />
-        <span class="blur-grid-item__desc">blurQuality=0.5</span>
-      </div>
-    </div>
+    <img
+      style="width: 728px; height: 1007px"
+      :src="
+        require('@/assets/Macaca_nigra_self-portrait_large.jpg?placeholder&format=blurred-svg&blurQuality=0.5')
+      "
+    />
   </div>
 </template>
-
-<style>
-.blur-container {
-  margin-top: 2rem;
-}
-
-.blur-title {
-  margin-top: 0;
-  margin-bottom: 1em;
-  text-align: center;
-}
-
-.blur-grid {
-  display: flex;
-  justify-content: space-between;
-}
-
-.blur-grid-item {
-  display: flex;
-  flex-direction: column;
-  width: calc(20% - 5px);
-}
-
-.blur-grid-item__desc {
-  display: block;
-  margin-top: 10px;
-  font-size: 20px;
-  font-weight: 500;
-  text-align: center;
-}
-
-.blur-grid-item__img {
-  display: block;
-  width: 100%;
-  height: auto;
-}
-</style>
